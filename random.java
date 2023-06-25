d903a66bf41ce58b92e4b5fa632927efeb70d8c2
PrabhuRaj2001 <prabhuraj152001@gmail.com>
2023-06-25 23:38:09 +0530

    sucessfully added

---
+++ random.java
@@ -0,0 +1,16 @@
+package sample;
+import java.util.*;
+public class prabhu {
+	
+	static int a=12;
+
+	public static void main(String[] args) throws Exception {
+		
+		double res=9999*Math.random()+9999;
+		int otp=(int)res;
+		System.out.println(otp);
+		
+	}
+	
+	
+}
